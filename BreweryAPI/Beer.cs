5a9af3ea5e2ec823e1df71cd2f3f82d0e060fe0d
Hy Huynh <hyxhuynh@gmail.com>
2020-12-16 20:31:12 -0800

    Add project files.

---
+++ BreweryAPI/Beer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BreweryAPI
+{
+    public class Beer
+    {
+        public Beer()
+        {
+
+        }
+
+    }
+}
